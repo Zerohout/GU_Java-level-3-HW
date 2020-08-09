4e9d1210d84659bc5612d7c95fdd968ae25c1a3c
Zerohout <zerohout@gmail.com>
2020-08-10 01:08:30 +0300

    lesson 4 part 1 is done

--- lesson4/src/main/java/Lesson4.java
+++ lesson4/src/main/java/Lesson4.java
@@ -1,15 +1,63 @@
 public class Lesson4 {
-    public static void main(String[] args) {
-        var dbr = new DBRequestBuilder();
-        System.out.println(dbr.insert("login","tester").insert("password","123").insert("nickname","Tester").insert("isOnline",true).buildInsert().build());
-        System.out.println(dbr.reset().update("nickname","Moder").update("isOnline",false).where("login","client2").where("password","pass2").build());
-        System.out.println(dbr.reset().delete().where("nickname","NonModer").where("id",5).build());
-        System.out.println(dbr.reset().select("id","nickname").where("login","Moder").build());
-        System.out.println(dbr.reset().select().where("login","Moder").build());
-        System.out.println(dbr.reset().delete().build());
-        System.out.println(dbr.reset().delete().where("id",3).build());
+    private final Object monitor = new Object();
 
+    public static void main(String[] args) {
+        var lesson4 = new Lesson4();
+        lesson4.start();
     }
 
+    private static State currentState = State.WAIT_A;
 
+    enum State {
+        WAIT_A,
+        WAIT_B,
+        WAIT_C
+    }
+
+    public void start() {
+        createThread("A").start();
+        createThread("B").start();
+        createThread("C").start();
+    }
+
+    private Thread createThread(String letter) {
+        State state;
+        State nextState;
+        switch (letter) {
+            case "A" -> {
+                state = State.WAIT_A;
+                nextState = State.WAIT_B;
+            }
+            case "B" -> {
+                state = State.WAIT_B;
+                nextState = State.WAIT_C;
+            }
+            case "C" -> {
+                state = State.WAIT_C;
+                nextState = State.WAIT_A;
+            }
+            default -> throw new RuntimeException("switch error. Unknown entered variable");
+        }
+
+        return new Thread(() -> printLetter(letter,state,nextState,Thread.currentThread()));
+    }
+
+    private void printLetter(String letter, State state, State nextState, Thread thread) {
+        synchronized (monitor) {
+            for (var i = 0; i < 5; ) {
+                if (currentState == state) {
+                    System.out.print(letter);
+                    i++;
+                    currentState = nextState;
+                    monitor.notifyAll();
+                } else {
+                    try {
+                        monitor.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
 }
